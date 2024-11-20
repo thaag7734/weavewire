ff76e73b3e1d7f8c7f7fb591f4f5f16801a51311
thaag7734 <thaag7734@gmail.com>
2024-11-20 12:43:48 -0500

    remove another unused import

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -8,7 +8,6 @@ use App\Models\Reaction;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
