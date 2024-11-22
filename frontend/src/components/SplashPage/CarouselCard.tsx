114876194802310bb134415d5ac9694ae1fe95f8
thaag7734 <thaag7734@gmail.com>
2024-11-22 03:33:05 -0500

    incomplete styling/fucntionality for splash page

---
+++ frontend/src/components/SplashPage/CarouselCard.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef, useState } from "react";
+import { selectPostById } from "../../redux/reducers/posts";
+import { useAppSelector } from "../../redux/util";
+import "./CarouselCard.css"
+
+export default function CarouselCard({ postId }: { postId: number }) {
+  const post = useAppSelector((state) => selectPostById(state, postId));
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="carousel-card card" ref={cardRef}>
+      <div className="carousel-card-img">
+        {post
+          ? `https://static.weavewire.tylerhaag.dev/images/${post.image_file}`
+          : null}
+      </div>
+      <p className="carousel-card-caption">{post?.caption}</p>
+    </div>
+  );
+}
--- frontend/src/components/SplashPage/SplashPage.tsx
+++ frontend/src/components/SplashPage/SplashPage.tsx
@@ -1,11 +1,21 @@
-import { type FormEvent, useEffect, useState } from "react";
+import React, { type FormEvent, useEffect, useState } from "react";
 import { csrfFetch } from "../../util/csrfFetch";
+import { useAppDispatch, useAppSelector } from "../../redux/util";
+import { getRecentPosts, selectAllPosts } from "../../redux/reducers/posts";
+import CarouselCard from "./CarouselCard";
+import "./SplashPage.css";
 
 export default function SplashPage() {
+  const posts = useAppSelector(state => selectAllPosts(state));
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const dispatch = useAppDispatch();
 
-  const handleSubmit = async (e: FormEvent) => {
+  useEffect(() => {
+    dispatch(getRecentPosts());
+  }, [])
+
+  const handleLogin = async (e: FormEvent) => {
     e.preventDefault();
 
     await csrfFetch("/auth/login", {
@@ -18,23 +28,58 @@ export default function SplashPage() {
     });
   };
 
+  const handleAnimEnd = (e: React.AnimationEvent) => {
+    const target = e.currentTarget;
+
+    e.currentTarget.parentElement?.appendChild(target)
+  }
+
   return (
-    <form method="POST" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="email"
-        value={email}
-        onChange={(e) => setEmail(e.currentTarget.value)}
-        placeholder="email"
-      />
-      <input
-        type="text"
-        name="password"
-        value={password}
-        onChange={(e) => setPassword(e.currentTarget.value)}
-        placeholder="password"
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="splash">
+      <div className="carousel-group" onAnimationEnd={handleAnimEnd}>
+        <div className="carousel">
+          {posts.map((post) =>
+            <CarouselCard key={post.id} postId={post.id} />
+          )}
+        </div>
+        <div className="carousel">
+          {posts.map((post) =>
+            <CarouselCard key={post.id} postId={post.id} />
+          )}
+        </div>
+      </div>
+      <div className="splash-cta-group">
+        <div className="splash-cta">
+          <h1>Weavewire</h1>
+          <p>ermmmmm make an account, stinky</p>
+        </div>
+        <div className="splash-login card">
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <div className="input">
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.currentTarget.value)}
+                />
+              </div>
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <div className="input">
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                />
+              </div>
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
