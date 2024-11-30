68b3d0158f0a46a8e3ea3752c4a01a3fc087b298
thaag7734 <thaag7734@gmail.com>
2024-11-30 01:19:38 -0500

    redirect user to feed if logged in on splash page

--- frontend/src/components/SplashPage/SplashPage.tsx
+++ frontend/src/components/SplashPage/SplashPage.tsx
@@ -4,76 +4,84 @@ import { useAppDispatch, useAppSelector } from "../../redux/util";
 import { getRecentPosts, selectOrderedPosts } from "../../redux/reducers/posts";
 import CarouselCard from "./CarouselCard";
 import "./SplashPage.css";
-import { login } from "../../redux/reducers/session";
+import { login, restoreUser } from "../../redux/reducers/session";
+import { useNavigate } from "react-router-dom";
 
 export default function SplashPage() {
-  const posts = useAppSelector((state) => selectOrderedPosts(state));
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const dispatch = useAppDispatch();
+	const posts = useAppSelector((state) => selectOrderedPosts(state));
+	const user = useAppSelector((state) => state.session.user);
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(getRecentPosts());
-  }, [dispatch]);
+	useEffect(() => {
+		if (user) navigate("/feed");
+	}, [user, navigate]);
 
-  const handleLogin = async (e: FormEvent) => {
-    e.preventDefault();
+	useEffect(() => {
+		dispatch(restoreUser());
+		dispatch(getRecentPosts());
+	}, [dispatch]);
 
-    dispatch(login({ email, password }));
-  };
+	const handleLogin = async (e: FormEvent) => {
+		e.preventDefault();
 
-  const handleAnimEnd = (e: React.AnimationEvent) => {
-    const target = e.currentTarget;
+		dispatch(login({ email, password }));
+	};
 
-    e.currentTarget.parentElement?.appendChild(target);
-  };
+	const handleAnimEnd = (e: React.AnimationEvent) => {
+		const target = e.currentTarget;
 
-  return (
-    <div className="splash">
-      <div className="carousel-group" onAnimationEnd={handleAnimEnd}>
-        <div className="carousel">
-          {posts.map((post) => (
-            <CarouselCard key={post.id} postId={post.id} />
-          ))}
-        </div>
-        <div className="carousel">
-          {posts.map((post) => (
-            <CarouselCard key={post.id} postId={post.id} />
-          ))}
-        </div>
-      </div>
-      <div className="splash-cta-group">
-        <div className="splash-cta">
-          <h1>Weavewire</h1>
-          <p>ermmmmm make an account, stinky</p>
-        </div>
-        <div className="splash-login card">
-          <h2>Login</h2>
-          <form onSubmit={handleLogin}>
-            <div className="input-group">
-              <label htmlFor="email">Email</label>
-              <div className="input">
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.currentTarget.value)}
-                />
-              </div>
-            </div>
-            <div className="input-group">
-              <label htmlFor="password">Password</label>
-              <div className="input">
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.currentTarget.value)}
-                />
-              </div>
-            </div>
-            <button type="submit">Login</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+		e.currentTarget.parentElement?.appendChild(target);
+	};
+
+	return (
+		<div className="splash">
+			<div className="carousel-group" onAnimationEnd={handleAnimEnd}>
+				<div className="carousel">
+					{posts.map((post) => (
+						<CarouselCard key={post.id} postId={post.id} />
+					))}
+				</div>
+				<div className="carousel">
+					{posts.map((post) => (
+						<CarouselCard key={post.id} postId={post.id} />
+					))}
+				</div>
+			</div>
+			<div className="splash-cta-group">
+				<div className="splash-cta">
+					<h1>Weavewire</h1>
+					<p>ermmmmm make an account, stinky</p>
+				</div>
+				<div className="splash-login card">
+					<h2>Login</h2>
+					<form onSubmit={handleLogin}>
+						<div className="input-group">
+							<label htmlFor="email">Email</label>
+							<div className="input">
+								<input
+									type="email"
+									value={email}
+									onChange={(e) => setEmail(e.currentTarget.value)}
+								/>
+							</div>
+						</div>
+						<div className="input-group">
+							<label htmlFor="password">Password</label>
+							<div className="input">
+								<input
+									type="password"
+									value={password}
+									onChange={(e) => setPassword(e.currentTarget.value)}
+								/>
+							</div>
+						</div>
+						<button type="submit">Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
